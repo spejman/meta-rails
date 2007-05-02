09b0b59771339109b9d0ed2a947d2ae622552452
spejman <spejman@14a41de8-8a15-4bb7-9f6c-6c1b07465e8c>
2007-05-02 11:54:59 +0000

    * Add custom stylesheet and automatic install via install.rb
    git-svn-id: svn+ssh://rubyforge.org/var/svn/meta-rails/HEAD@30 14a41de8-8a15-4bb7-9f6c-6c1b07465e8c

--- meta_querier/install.rb
+++ meta_querier/install.rb
@@ -1,1 +1,7 @@
 # Install hook code here
+RAILS_ROOT = "./"
+require "fileutils"
+
+destination = File.join(RAILS_ROOT, "public/stylesheets/meta_rails")
+FileUtils.mkdir(destination) unless File.exist?(destination)
+FileUtils.cp(File.join(RAILS_ROOT, "vendor/plugins/meta_querier/files/meta_querier.css"), destination)
