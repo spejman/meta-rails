0ca3eac71a59f5a020e8ebe71c3eb836cf4904c0
spejman <spejman@14a41de8-8a15-4bb7-9f6c-6c1b07465e8c>
2007-05-21 11:54:57 +0000

    * active_scaffold: add the image of the current model.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/meta-rails/HEAD@52 14a41de8-8a15-4bb7-9f6c-6c1b07465e8c

--- meta_scaffold/generators/meta_scaffold/templates/active_scaffold_controller.rb
+++ meta_scaffold/generators/meta_scaffold/templates/active_scaffold_controller.rb
@@ -1,4 +1,11 @@
 class MetaScaffoldModels::<%= class_name.camelize.pluralize %>Controller < ApplicationController
     layout "meta_scaffold"
 	active_scaffold :<%= class_name.underscore.singularize %>
+  
+  before_filter :set_meta_scaffold_class_name
+
+  def set_meta_scaffold_class_name
+    @meta_scaffold_class_name = "<%= class_name.camelize.singularize %>"
+  end
+
 end
