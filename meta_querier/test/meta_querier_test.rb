351a1653f94ebf7be18163987bf115c3f347de70
spejman <spejman@14a41de8-8a15-4bb7-9f6c-6c1b07465e8c>
2007-07-06 12:24:54 +0000

    * Added tests to meta_querier
    git-svn-id: svn+ssh://rubyforge.org/var/svn/meta-rails/HEAD@116 14a41de8-8a15-4bb7-9f6c-6c1b07465e8c

--- meta_querier/test/meta_querier_test.rb
+++ meta_querier/test/meta_querier_test.rb
@@ -1,8 +1,14 @@
 require 'test/unit'
+require File.dirname(__FILE__) + '/../../../../test/test_helper'
 
 class MetaQuerierTest < Test::Unit::TestCase
   # Replace this with your real tests.
-  def test_this_plugin
-    flunk
+  def test_installation
+    assert File.exist?(File.join(RAILS_ROOT, "public/stylesheets/meta_rails/meta_querier.css"))
+    assert File.exist?(File.join(RAILS_ROOT, "public/images/meta_rails/cross.png"))
+    assert File.exist?(File.join(RAILS_ROOT, "public/images/meta_rails/indicator.gif"))
+    assert File.exist?(File.join(RAILS_ROOT, "public/images/meta_rails/add.png"))
+    assert File.exist?(File.join(RAILS_ROOT, "public/images/meta_rails/remove.png"))
+    assert File.exist?(File.join(RAILS_ROOT, "public/images/meta_rails/meta_querier"))    
   end
 end
